bc96aab90d9a5b04b621d6e31e72815605d6f2d5
Sanchitha Kuthethoor <skuthethoor02@gmail.com>
2025-04-30 13:04:14 -0400

    Deleted few more files

--- models/xgboost.py
+++
@@ -1,6 +0,0 @@
-import xgboost as xgb
-
-def train_xgboost(params={},data={}):
-    model = xgb.XGBRegressor(objective=params['objective'], n_estimators=params['n_estimators'], learning_rate=params['lr'], max_depth=params['max_depth'])
-    model.fit(data['X'], data['y'])
-    return model
